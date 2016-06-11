7f8c56fa04ca7178116564067c892a1b355f6933
feng1o <liufeng_mail@126.com>
2016-06-12 01:01:28 +0800

    xxx

---
+++ chp24-oob/tcpsend01.c
@@ -0,0 +1,40 @@
+#include	"../lib/unp.h"
+#include"../lib/error.c"
+#include"../lib/wrapsock.c"
+#include"../lib/tcp_connect.c"
+#include"../lib/wrapunix.c"
+#include"../lib/wrapstdio.c"
+#include"../lib/wraplib.c"
+#include"../lib/writen.c"
+
+int main(int argc, char **argv)
+{
+	int		sockfd;
+
+	if (argc != 3)
+		err_quit("usage: tcpsend01 <host> <port#>");
+
+	sockfd = Tcp_connect(argv[1], argv[2]);
+
+	Write(sockfd, "123", 3);
+	printf("wrote 3 bytes of normal data\n");
+	sleep(1);
+
+	Send(sockfd, "4", 1, MSG_OOB);
+	printf("wrote 1 byte of OOB data\n");
+	sleep(1);
+
+	Write(sockfd, "56", 2);
+	printf("wrote 2 bytes of normal data\n");
+	sleep(1);
+
+	Send(sockfd, "7", 1, MSG_OOB);
+	printf("wrote 1 byte of OOB data\n");
+	sleep(1);
+
+	Write(sockfd, "89", 2);
+	printf("wrote 2 bytes of normal data\n");
+	sleep(1);
+
+	exit(0);
+}
